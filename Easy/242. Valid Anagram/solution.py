49739da4fa259d0c0f250bd0abd2b0965bd83c31
Sivakumar <55843765+Arsive02@users.noreply.github.com>
2022-05-23 10:22:33 +0530

    Update solution.py

--- Easy/242. Valid Anagram/solution.py
+++ Easy/242. Valid Anagram/solution.py
@@ -2,6 +2,4 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if Counter(s) == Counter(t):
-            return True
-        return False
+        return Counter(s) == Counter(t)
