a7de48ecfbcb0a58d55bd01f148f82e24dd0d703
Sivakumar <55843765+Arsive02@users.noreply.github.com>
2022-06-06 18:55:09 +0530

    Create solution.py

---
+++ Easy/1189. Maximum Number of Balloons/solution.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = Counter(text)
+        k = Counter('balloon')
+        counter = 0
+        f = 0
+        while not f:
+            for x,y in k.items():
+                if d[x] - y < 0:
+                    f = 1
+                    break
+                d[x] = d[x] - y
+            if not f:
+                counter += 1
+        return counter
+        
+        
